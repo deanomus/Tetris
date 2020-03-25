d6fd76be7a8f4ca39494c899f3e3358063acc0b4
Dean Reinhardt <d_reinhardt02@hotmail.com>
2020-03-25 10:19:28 +0100

    Helper Class, rotate 2d Array

--- src/de/deanomus/tetris/util/Helper.java
+++ src/de/deanomus/tetris/util/Helper.java
@@ -1,5 +1,7 @@
 package de.deanomus.tetris.util;
 
+import java.util.Random;
+
 public class Helper {
 
     public static String ArrayToString(int[][] array) {
@@ -12,6 +14,53 @@ public class Helper {
             out += "\n";
         }
         return out;
+    }
+
+    public static int[][] rotateArray(int[][] Array) {
+        int[][] newArray = new int[Array.length][Array[0].length];
+        for (int i = 0; i < Array.length; i++) {
+            for (int j = 0; j < Array.length; j++) {
+                newArray[j][Array.length-1-i] = Array[i][j];
+            }
+        }
+
+        return newArray;
+    }
+
+    public static int[][] rotateArray(int Times, int[][] Array) {
+        if(Times <= 0) return Array;
+
+        int[][] newArray = new int[Array.length][Array[0].length];
+        for (int i = 0; i < Array.length; i++) {
+            for (int j = 0; j < Array[0].length; j++) {
+                newArray[i][j] = Array[i][j];
+            }
+        }
+
+        for (int i = 0; i < Times; i++) {
+            newArray = rotateArray(newArray);
+        }
+        return newArray;
+    }
+
+
+    public static int rdmInt(int min, int max) {
+        Random r = new Random();
+        int genInt = r.nextInt((max - min) + 1) + min;
+        return genInt;
+    }
+
+    public static int NegativePositiveChange(int i) {
+        if(i == 0) return i;
+
+        if(i < 0) {
+            return Math.abs(i);
+        }
+
+        if(i > 0) {
+            return (i - (i * 2));
+        }
+        return 0;
     }
 
 }
